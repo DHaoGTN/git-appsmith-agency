1d25a6f65de29dc0d7e436375d4ed27431fa86e9
tranbahuy9797 <tranbahuy9797@gmail.com>
2023-12-26 07:54:24 +0000

    as

--- pages/Agency_Form_All/jsobjects/app_function/app_function.js
+++ pages/Agency_Form_All/jsobjects/app_function/app_function.js
@@ -34,15 +34,54 @@ export default {
 		if (current_email === new_email){
 			return showAlert('Your new email is same with current email', 'error')
 		}
-		else if (count_new_email>0){
+		if (count_new_email>0){
 			return showAlert('Your new email have existed, please choose another email or login', 'error')
 		}
-		else {
-			await change_email.run({new_email, current_email})
-			await this.logout()
-			return showAlert('changed email successfully,  please login again', 'success')
-		}		
 
+		await change_email.run({new_email, current_email})
+		await this.logout()
+		return showAlert('changed email successfully,  please login again', 'success')
+
+	},
+
+	createHash : (password)=>{
+		return dcodeIO.bcrypt.hashSync(password,10 );
+	},
+
+	verifyHash : (password, hash ) =>{
+		return dcodeIO.bcrypt.compareSync(password, hash);
+	},
+
+	changePassword: async()=>{
+		const email = appsmith.store.email
+		const [user] = await find_user_by_email.run({email});
+
+		//user type passwod
+		const current_password_typing=  current_password_input.text;
+		const new_password_typing = confirm_password_input.text
+
+
+		// compare type password with db password
+		if ( user && this.verifyHash(current_password_typing, user?.hash)){
+			const current_password= user.hash;
+			console.log('password same')
+			//change password logic
+			// create new password hash
+			const new_password = this.createHash(new_password_typing)
+			return await change_password.run({new_password,current_password, email })
+				.then(()=>{ showAlert('password change', 'success')})
+				.catch((e)=>{ showAlert(e, 'error')})
+
+			//store new hasht to selected email and old hash
+
+		}  else {
+			console.log('password not same')
+			showAlert('Password is incorrect, please type again.')
+
+		}
+		// if same , change password
+
+		// else showalert
 	},
 
 
