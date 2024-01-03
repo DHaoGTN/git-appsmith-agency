5545addb6b3542704e17a015373b940e5f917c78
D Hao <d.hao@gtn-vietnam.com>
2024-01-03 04:26:00 +0000

    add send mail

--- pages/Agency_Form_All/jsobjects/form/form.js
+++ pages/Agency_Form_All/jsobjects/form/form.js
@@ -3,7 +3,7 @@ export default {
 	isWifiAllowed: null,
 	isCardAllowed: null,
 	isUtilitiesAllowed: null,
-	saveResultCode: 0,
+	resultCode: 0,
 	VALIDATE_NO_CHOICE: 0,
 	VALIDATE_CHOICE_ENOUGH: 1,
 	VALIDATE_CHOICE_NOT_ENOUGH: 2,
@@ -29,9 +29,56 @@ export default {
 	},
 
 	async submitForm() {
-		if (this.validateFormAll())
+		if (this.validateFormAll()) {
 			await this.saveApplicationAll();
+			await this.sendToAutoFill();
+			await this.sendEmailToPoC();
+		}
 	},
+	async sendToAutoFill() {
+		// TODO: Later, call RPA api
+	},
+	async sendEmailToPoC() {
+		let titleEmailGTN = 'Affiliate test title GTN';
+		let titleEmailAgency = 'Affiliate test title Agency';
+		let titleEmailCustomer = 'Affiliate test title Customer';
+		let bodyEmailGTN = 'Affiliate test body GTN';
+		let bodyEmailAgency = 'Affiliate test body Agency';
+		let bodyEmailCustomer = 'Affiliate test body Customer';
+		
+		let userInfo = await appsmith.store.user;
+		if (userInfo) {
+			const agencyId = JSON.stringify(userInfo.agency_id);
+			const currentEmail = JSON.stringify(userInfo.email); // current agency account
+			let agencyEmail = '';
+			await find_agency_email.run({agencyId})
+		    .then(res => agencyEmail = JSON.stringify(res[0].email))
+			  .catch(e => {this.resultCode = 71; showAlert(e.message, 'error');});
+			
+			// 1. Send to Agency
+			await this.sendEmail(currentEmail+', '+agencyEmail, titleEmailAgency, bodyEmailAgency, 72);
+			// 2. Send to GTN
+			await this.sendEmail('d.hao@gtn-vietnam.com', titleEmailGTN, bodyEmailGTN, 73);
+		}
+		let customerEmail = email_input.text;
+		// 3. Send to Customer
+		await this.sendEmail(customerEmail, titleEmailCustomer, bodyEmailCustomer, 74);
+		
+		if (this.resultCode == 0) {
+			showAlert('Email was sent successfully.', 'success');
+		} else {
+			showAlert('There are an error when sending email. Please try again. Result code: '+this.resultCode, 'error');
+		}
+	},
+	async sendEmail(emails, title, body, resultCodeIfFailed) {
+		if (emails == '' || title == '' || body == '') {
+			return false;
+		}
+		await send_emails.run({emails, title, body})
+		  .then()
+			.catch(e => {this.resultCode = resultCodeIfFailed; showAlert(e.message, 'error');});
+	},
+	
 	validateFormAll:() =>{
 		if (this.validateFormApplicant() == this.VALIDATE_CHOICE_ENOUGH && 
 				(this.validateFormWifi() == this.VALIDATE_CHOICE_ENOUGH || 
@@ -66,7 +113,7 @@ export default {
 		) {
 			return this.VALIDATE_CHOICE_NOT_ENOUGH;
 		}
-		if (!this.validateEmail(email_input.text) || email_input.text != email_cf_input.text)
+		if (email_input.text != email_cf_input.text)
 			return this.VALIDATE_DATA_INCORRECT;
 		return this.VALIDATE_CHOICE_ENOUGH;
 	},
@@ -141,10 +188,10 @@ export default {
 				}
 			}
 		}
-		if (this.saveResultCode == 0) {
-			showAlert('Save information success.', 'success');
+		if (this.resultCode == 0) {
+			showAlert('Application created successfully. We will send email to person in charge on GTN, Agency and Customer.', 'success');
 		} else {
-			showAlert('There are an error when saving information. Please try again. Result code: '+this.saveResultCode, 'error');
+			showAlert('There are an error when saving information. Please try again. Result code: '+this.resultCode, 'error');
 		}
 	},
 	async saveApplicant() {
@@ -167,7 +214,7 @@ export default {
 
 		await insert_applicant.run({fistname, lastname, fistnameKtkn, lastnameKtkn, birthday, nationality, visa, desiredLang, phone, email})
 		  .then(res => applicantId = JSON.stringify(res[0].id))
-			.catch(e => {this.saveResultCode = 1; showAlert(e.message, 'error');});
+			.catch(e => {this.resultCode = 1; showAlert(e.message, 'error');});
 		return applicantId;
 	},
 	
@@ -180,7 +227,7 @@ export default {
 		let room = address5_input.text;
 		await insert_address.run({applicantId, postalCode, prefecture, city, addressDetail, building, room})
 		  .then()
-			.catch(e => {this.saveResultCode = 2; showAlert(e.message, 'error');});
+			.catch(e => {this.resultCode = 2; showAlert(e.message, 'error');});
   },
 	
 	async saveApplications(applicantId) {
@@ -196,9 +243,9 @@ export default {
 			
 			await insert_application.run({agencyId, applicantId, serviceTypeCodes})
 				.then(res => applicationId = JSON.stringify(res[0].id))
-				.catch(e => {this.saveResultCode = 3; showAlert(e.message, 'error');});
+				.catch(e => {this.resultCode = 3; showAlert(e.message, 'error');});
 		} else {
-			this.saveResultCode = 31; // Can not get userInfo in storage
+			this.resultCode = 31; // Can not get userInfo in storage
 		}
 		return applicationId;
 	},
@@ -219,9 +266,9 @@ export default {
 		if (isUploadFrontSuccess && isUploadFrontSuccess) {
 			await insert_wifi_application.run({applicationId, contactDow, urlFront, urlBack, statusWifi})
 			  .then()
-				.catch(e => {this.saveResultCode = 4; showAlert(e.message, 'error');});
+				.catch(e => {this.resultCode = 4; showAlert(e.message, 'error');});
 		} else {
-			this.saveResultCode = 41;
+			this.resultCode = 41;
 		}
 	},
 	
@@ -229,7 +276,7 @@ export default {
 		let statusCard = '1.未対応';
 		await insert_card_application.run({applicationId, statusCard})
 		  .then()
-			.catch(e => {this.saveResultCode = 5; showAlert(e.message, 'error');});
+			.catch(e => {this.resultCode = 5; showAlert(e.message, 'error');});
 	},
 	
 	async saveUtilityApplications(applicationId) {
@@ -242,7 +289,7 @@ export default {
 		let statusUtility = '1.未対応';
 		await insert_utility_application.run({applicationId, utilityTypeCode, contractTypeCodes, electricityStartDate, gasStartDate, gasStartTimeCode, withWaterSupply, statusUtility})
 		  .then()
-			.catch(e => {this.saveResultCode = 6; showAlert(e.message, 'error');});
+			.catch(e => {this.resultCode = 6; showAlert(e.message, 'error');});
 	},
 
 	/**
