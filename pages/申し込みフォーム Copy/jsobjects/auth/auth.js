713f0860c339389269ec87df40767d5ad34b434f
tranbahuy9797 <tranbahuy9797@gmail.com>
2024-02-05 09:56:06 +0000

    asd

---
+++ pages/申し込みフォーム Copy/jsobjects/auth/auth.js
@@ -0,0 +1,122 @@
+export default {
+	secretBE: 'secret',
+	baseUrlBE: 'http://affiliate-stg-api.gtn.co.jp/rpa/',
+	getSecretBE: this.baseUrlBE+'get_secret',
+	rpaBE: this.baseUrlBE+'application_forms',
+	jwtBE: '',
+	async createTokenBE() {
+		let payload = {
+			// 'exp': 3600,
+			'scope': 'agency'
+		};
+		this.jwtBE = await jsonwebtoken.sign(payload, this.secretBE, {expiresIn: 3600});
+		// console.log('jwtBE: '+this.jwtBE);
+	},
+	
+	
+	////----------- AUTHENTICATION FUNCTION------------////
+	async startUp() {
+		const token = await appsmith.store.token;
+		const tokenInDb = await check_token_exist.run({ token });
+		const tokenCountInDb = tokenInDb[0]["count"];
+
+		if (token !== undefined && tokenCountInDb === 1) {
+			console.log("do nothing");
+		} else if (
+			token === undefined &&
+			tokenCountInDb === 0 &&
+			appsmith.mode !== "EDIT"
+		) {
+			navigateTo("ログイン");
+		}
+	},
+
+	isTokenValid: () => {
+		const token = appsmith.store.token;
+		try {
+			jsonwebtoken.verify(token, "gtn-id");
+			return true;
+		} catch (error) {
+			return false;
+		}
+	},
+
+	logout: async () => {
+		const token = appsmith.store.token;
+		// check_token_exist.run({token});
+		// use try catch incase there is token in local storage to compare with token in db
+		try {
+			await delete_token_in_db.run({ token });
+			await window.localStorage.clear();
+		} catch (error) {
+			return navigateTo("ログイン");
+		}
+		return showAlert(messages.Auth.LOGOUT_SUCCESS, "success")
+			.then(() => navigateTo("ログイン"))
+			.catch((e) => showAlert(e.message, "error"));
+	},
+
+	checkToken: () => {
+		const token = appsmith.store.token;
+		console.log(token);
+	},
+
+	////-------------------main loop ---------------------////
+	intervalId: null,
+	async checkValidTokenInDb() {
+		try {
+			const isTokenValid = this.isTokenValid();
+			if (isTokenValid) {
+				console.log("token still valid, continue");
+			} else {
+				console.log("token exprire, delete token in db");
+				const token = appsmith.store.token;
+				delete_token_in_db.run({ token });
+				showAlert(messages.Auth.SESSION_EXPIRED, "error");
+				console.log("deleted token in db ");
+			}
+		} catch (error) {
+			showAlert(messages.Auth.GENERAL_ERROR + "\n" + error.message, "error");
+		}
+	},
+
+	async loopToCheckTokenExist() {
+		if (!this.intervalId) {
+			const process = async () => {
+				try {
+					const token = appsmith.store.token;
+					console.log("token", token);
+
+					//check if current token is still valid or not
+					this.checkValidTokenInDb();
+
+					// if token is not valid, count in db is 0, else do nothing
+					const tokenInDb = await check_token_exist.run({ token });
+					console.log("token in db", tokenInDb);
+					const tokenCountInDb = tokenInDb[0]["count"];
+					console.log("tokenCountInDb", tokenCountInDb);
+					// if ( tokenCountInDb === 0 && appsmith.mode !== 'EDIT' ){
+					if (tokenCountInDb === 0) {
+						showAlert(messages.Error.SESSION_EXPIRED,'error')
+						this.logout();
+						console.log("navigate to ログイン");
+					}
+					console.log("token still valid");
+				} catch (error) {
+					showAlert(
+						messages.Auth.GENERAL_ERROR + "\n" + error.message,
+						"error"
+					);
+				}
+			};
+			this.intervalId = setInterval(process, 5000);
+		}
+	},
+
+	stopCountingAndLogging() {
+		if (this.intervalId) {
+			clearInterval(this.intervalId);
+			this.intervalId = null;
+		}
+	},
+};
---
+++ pages/申し込みフォーム Copy/jsobjects/messages/messages.js
@@ -0,0 +1,22 @@
+export default {
+	Auth: {
+		LOGOUT_SUCCESS: 'You have been logged out.',
+		SESSION_EXPIRED: 'Your session have been expired, please log in again.',
+		GENERAL_ERROR: 'Something wrong, please login again!',
+	},
+	Form: {
+		RESULT_CODE: ' Result code: ',
+		GET_SERVICE_TYPE_FAILURE: 'Can not retrieve service type code.',
+		EMAIL_SEND_SUCCESS: 'メール送信が成功しました！　Email was sent successfully!',
+		EMAIL_SEND_FAILURE: 'メール送信が失敗しました！　There are an error when sending email. Please try again.',
+		VALIDATE_ERROR_GENERAL: 'Please fill all of required fields and check data correctly.',
+		APPLICATION_CREATED_SUCCESS: 'Application was created successfully. We will inform by sending email to person in charge for this application.',
+		APPLICATION_CREATED_FAILURE: 'There are an error when saving information. Please try again.',
+	},
+	Other: {
+		COPY_LINK_SUCCESS: 'Link was copied to clipboard!',
+	},
+	Error: {
+		SESSION_EXPIRED :'Your session have been expired, please log in again.',
+	},
+}
